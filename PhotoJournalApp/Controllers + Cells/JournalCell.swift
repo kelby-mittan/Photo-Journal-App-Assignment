96b949482246d0eb754b7ad48c16f94eeef947f7
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-23 18:16:20 -0500

    set up custom cell for photos

---
+++ PhotoJournalApp/Controllers + Cells/JournalCell.swift
@@ -0,0 +1,30 @@
+//
+//  JournalCell.swift
+//  PhotoJournalApp
+//
+//  Created by Kelby Mittan on 1/23/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class JournalCell: UICollectionViewCell {
+    @IBOutlet var photoImage: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+//    public var imageObject: ImageObject?
+    
+    func configureCell(for photo: ImageObject) {
+        
+        descriptionLabel.text = photo.description
+        dateLabel.text = photo.date.description
+        
+        guard let image = UIImage(data: photo.imageData) else {
+            return
+        }
+        photoImage.image = image
+        
+    }
+    
+}
