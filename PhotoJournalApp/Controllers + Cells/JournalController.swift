66b61ee108fbfa1469e89212c3e411f5faf27d93
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-23 14:49:32 -0500

    Initial Commit

---
+++ PhotoJournalApp/Controllers + Cells/JournalController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournalApp
+//
+//  Created by Kelby Mittan on 1/23/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class JournalController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
